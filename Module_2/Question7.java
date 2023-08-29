79f8479a3d3dd4a8ff4cbab33bf9bdb210328484
Divyangjainc <113012854+Divyangjainc@users.noreply.github.com>
2023-08-29 15:25:39 +0530

    Add files via upload

---
+++ Module_2/Question7.java
@@ -0,0 +1,7 @@
+package Module_2;
+//Create a class named 'PrintNumber' to print various numbers of different data 
+//types by creating different methods with the same name 'printn' having a 
+//parameter for each data type. 
+public class Question7 {
+
+}
