02813697795647b38b60608a850842d2520b5548
Divyangjainc <113012854+Divyangjainc@users.noreply.github.com>
2023-10-21 14:58:26 +0530

    Add files via upload

---
+++ dao/SellerDao.java
@@ -0,0 +1,115 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import connection.DBConnection;
+import model.Seller;
+
+public class SellerDao {
+	public static void insertSeller(Seller s) {
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql = "insert into seller(name,contact,address,email,password) values(?,?,?,?,?)";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, s.getName());
+			pst.setLong(2, s.getContact());
+			pst.setString(3, s.getAddress());
+			pst.setString(4, s.getEmail());
+			pst.setString(5, s.getPassword());
+			pst.executeUpdate();
+			System.out.println("data insertd");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static boolean checkEmail(String email) {
+		boolean flag = false;
+		try {
+			Connection con = DBConnection.createConnection();
+			String sql = "select * from seller where email=?";
+			PreparedStatement pst = con.prepareStatement(sql);
+			pst.setString(1, email);
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				flag = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+	public static Seller userLogin(String email, String pass) {
+		Seller u = null;
+		try {
+			Connection con = DBConnection.createConnection();
+			String sql = "select * from seller where email=? and password=?";
+			PreparedStatement pst = con.prepareStatement(sql);
+			pst.setString(1, email);
+			pst.setString(2, pass);
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				u = new Seller();
+				u.setId(rs.getInt("id"));
+				u.setName(rs.getString("name"));
+				u.setContact(rs.getLong("contact"));
+				u.setAddress(rs.getString("address"));
+				u.setEmail(rs.getString("email"));
+				u.setPassword(rs.getString("password"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return u;
+	}
+	public static void updateSellerProfile(Seller s) {
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql = "update seller set name=?,contact=?,address=?,email=? where id=?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, s.getName());
+			pst.setLong(2, s.getContact());
+			pst.setString(3, s.getAddress());
+			pst.setString(4, s.getEmail());
+			pst.setInt(5, s.getId());
+			pst.executeUpdate();
+			System.out.println("data updated");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static boolean checkOldPassword(int id,String pass) {
+		boolean flag = false;
+		try {
+			Connection con = DBConnection.createConnection();
+			String sql = "select * from seller where id=? and password=?";
+			PreparedStatement pst = con.prepareStatement(sql);
+			pst.setInt(1, id);
+			pst.setString(2, pass);
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				flag = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	public static void updatePassword(int id,String np) {
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql = "update seller set password=? where id=?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, np);
+			pst.setInt(2, id);
+			pst.executeUpdate();
+			System.out.println("pass updated");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
